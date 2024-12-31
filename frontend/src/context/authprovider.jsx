54e6442d7452b4da727ba53a6a608fc9fc25d387
Naman <namanherwatta@gmail.com>
2024-12-31 22:29:02 +0530

    API Games corrected

--- frontend/src/context/authprovider.jsx
+++ frontend/src/context/authprovider.jsx
@@ -1,5 +1,6 @@
 import React, { Children, createContext, useContext, useEffect, useState } from 'react'
 import axios from "axios"
+import {BACKEND_URL} from "../utils.js"
 
 
 export const Authcontext=createContext()
@@ -13,7 +14,7 @@ export const AuthProvider= ({children})=> {
                 let token = localStorage.getItem("jwt"); // Retrieve the token directly from the localStorage (Go to login.jsx)
                console.log(token)
                 if(token){
-                const {data} = await axios.get("http://localhost:4001/api/users/myProfile", { withCredentials: true,headers: {
+                const {data} = await axios.get(BACKEND_URL+"/api/users/myProfile", { withCredentials: true,headers: {
                     "Content-Type": "application/json",
                   }, })
                 console.log(data)
@@ -26,7 +27,7 @@ export const AuthProvider= ({children})=> {
         }
         const fetchGames=async()=>{
             try {
-                const {data} = await axios.get("http://localhost:4001/api/games/allgames", { withCredentials: true })
+                const {data} = await axios.get(BACKEND_URL+"/api/games/allgames", { withCredentials: true })
                 console.log(data)
                 setGames(data)
             } catch (error) {
